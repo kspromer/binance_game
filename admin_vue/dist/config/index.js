f7c11313bd3e50ab4a06fd7065f82d58c230802a
liuyuchan286 <liuyuchan286@gmail.com>
2022-04-13 22:27:20 +0800

    init

--- admin_vue/dist/config/index.js
+++ admin_vue/dist/config/index.js
@@ -9,7 +9,7 @@
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
-  window.SITE_CONFIG['version'] = '2204042323';   // 版本号(年月日时分)
+  window.SITE_CONFIG['version'] = '2204132206';   // 版本号(年月日时分)
   window.SITE_CONFIG['cdnUrl']  = window.SITE_CONFIG.domain + window.SITE_CONFIG.version;
 })();
 
